8e895c2f9a5a878b84c23d751e13f75657157a0b
Anna <110714010+Trivija@users.noreply.github.com>
2022-12-15 19:43:38 +0300

    Update task_4.py

--- HW_2/task_4.py
+++ HW_2/task_4.py
@@ -1,8 +1,61 @@
-"""
-Задайте список из N элементов, заполненный целыми числами из промежутка [-N, N].
-Найдите произведение элементов на индексах, хранящихся в файле indexes.txt (в одной строке один индекс).
-Решение должно работать при любом натуральном N.
 
-Ввод: значение типа <int>
-Вывод: значение типа <int>
-"""
+# Задайте список из N элементов, заполненный целыми числами из промежутка [-N, N].
+# Найдите произведение элементов на индексах, хранящихся в файле indexes.txt (в одной строке один индекс).
+# Решение должно работать при любом натуральном N.
+
+# Ввод: значение типа <int>
+# Вывод: значение типа <int>
+
+import random
+from random import randint
+
+number = int(input('Введите натуральное число N = '))
+my_list = [randint(-number, number) for _ in range(number)]
+print(my_list)
+
+res = 1
+with open('/Users/annaponomareva/Python_START/Python_START/HW_2/indexes.txt', 'r') as file:
+
+    for line in file:
+        index = int(line)
+        if number > index >= -number:
+            res *= my_list[index]
+print(res)
+
+
+
+
+
+
+
+
+# # 2
+
+# from random import randint
+
+# number = int(input("Введите натуральное число: "))
+# my_list = [randint(-number, number)for _ in range(number)]
+# print(my_list)
+# res = 1
+# with open('indexes.txt', 'r') as file:
+#     for line in file:
+#         index = int(line)
+#         if len(my_list) > index >= -len(my_list):
+#             res *= my_list[index]
+# print(res)
+
+
+# # 3
+
+# num = int(input('введите число: '))
+# new_list = [random.randint(-num, num) for k in range(num)]
+# index_list = []
+# prod = 1
+# with open('index.txt', encoding='UTF-8') as data:
+#     for index in data:
+#         index_list.append(int(index))
+# for i in range(-num, num):
+#     if i in index_list:
+#         prod *= new_list[i]
+# print(prod)
+
